70d89a0ab47922d1d2d7193db4b5e5b2c072fb36
Cristián <caayala@gmail.com>
2021-04-08 12:49:00 -0400

    Ajuste de gráfico 08-mascotas.R

--- R/08-mascotas.R
+++ R/08-mascotas.R
@@ -18,29 +18,44 @@ mascotas <- tibble(REGION,
                    PERROS)
 
 mascotas <- mascotas %>% 
+  mutate(REGION = as_factor(REGION),
+         REGION = fct_rev(REGION)) %>% 
   group_by(REGION) %>% 
   mutate(TOTAL = GATOS + PERROS,
          prop_gato = GATOS/TOTAL,
-         prop_perro = PERROS/TOTAL)
+         prop_perro = PERROS/TOTAL,
+         prop_diff = prop_perro - prop_gato)
 
-gg_mascotas <- ggplot(data = mascotas) +
-  geom_segment(aes(x=prop_gato, xend=prop_perro, y=REGION, yend=REGION), color = 'grey50', size = 1.2) +
-  geom_point(aes(x=prop_perro, y=REGION, color = 'Perro'), size = 4) +
-  geom_point(aes(x=prop_gato, y=REGION, color = 'Gato'), size = 4) + 
-  scale_x_continuous('', limits = c(0,1), labels = function(x) scales::percent(x, accuracy = 1)) +
-  # scale_y_discrete(limits=c("RM", "Magallanes", "Ays?n", "Los Lagos", "Los R?os", "La Araucan?a", "Biob?o", "?uble", "Maule",
-  #                           "O?Higgins", "Valparaiso", "Coquimbo", "Atacama", "Antofagasta", "Tarapac?", "Arica y Parinacota")) +
+ggplot(data = mascotas,
+       aes(y = REGION)) +
+  geom_segment(aes(x=prop_gato, 
+                   xend=prop_perro, 
+                   yend = REGION,
+                   colour = prop_diff),
+               size = 1.2) +
+  geom_text(aes(x=prop_perro), 
+            label = '🐶',
+            size = 6) +
+  geom_text(aes(x=prop_gato), 
+            label = '🐱',
+            size = 6) + 
+  scale_x_continuous('', 
+                     limits = c(0,1), 
+                     labels = function(x) scales::percent(x, accuracy = 1)) +
   scale_shape_manual(values=c(19,20)) +
-  scale_colour_manual(values=c("#006633", "#000066")) +
+  scale_color_viridis_c(guide = 'none') + 
+  # scale_colour_manual(values=c("#006633", "#000066")) +
   theme_minimal() +
   theme(legend.title = element_blank(),
         plot.title = element_text(size = rel(1)),
         plot.subtitle = element_text(size = rel(0.8)),
         axis.text.y = element_text(size = 10)) +
-  labs(title = "Registro Nacional de Mascotas: Seg?n regi?n",
-       subtitle = "Proporci?n de mascotas registradas",
-       caption = "Datos obtenidos desde p?gina web de SUBDERE (2019)",
-       y ='')
+  labs(title = "Registro Nacional de Mascotas: Según región",
+       subtitle = "Proporción de mascotas registradas",
+       caption = "Datos obtenidos desde página web de SUBDERE (2019)",
+       y = NULL) +
+  theme(plot.title.position = 'plot',
+        plot.title = element_text(face = 'bold'))
 
 ggsave('lollipop_mascotas2.png',
        width = 5,
